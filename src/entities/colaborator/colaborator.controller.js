219743df9959f55c7963aa92c4bcdde4b482f99d
Kenneth Velásquez <kvelasquez-2019504@kinal.edu.gt>
2025-02-27 20:56:25 -0600

    funcion: se agrego paginacion de colaboradores

--- src/entities/colaborator/colaborator.controller.js
+++ src/entities/colaborator/colaborator.controller.js
@@ -3,37 +3,50 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const postColaborator = async (req = Request, res = Response) => {
-   try {
-    const { NOMBRE, APELLIDO, DIRECCION, PROFESION, ESTADOCIVIL } = req.body;
-    let { EDAD } = req.body;
-    EDAD = parseInt(EDAD);
-    const colaborator = await prisma.colaborador.create({
-        data: {
-            NOMBRE,
-            APELLIDO,
-            DIRECCION,
-            EDAD,
-            PROFESION,
-            ESTADOCIVIL
-        }
-    });
+    try {
+        const { NOMBRE, APELLIDO, DIRECCION, PROFESION, ESTADOCIVIL } = req.body;
+        let { EDAD } = req.body;
+        EDAD = parseInt(EDAD);
+        const colaborator = await prisma.colaborador.create({
+            data: {
+                NOMBRE,
+                APELLIDO,
+                DIRECCION,
+                EDAD,
+                PROFESION,
+                ESTADOCIVIL
+            }
+        });
 
-    res.status(200).json({
-        msg: "Se ha añadido un colaborador",
-        colaborator
-      });
-   } catch (error) {
-    console.log(error)
-   }
+        res.status(200).json({
+            msg: "Se ha añadido un colaborador",
+            colaborator
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const getColaborators = async (req = Request, res = Response) => {
     try {
-        const colaborators = await prisma.colaborador.findMany();
+        const { pageSize = 10, page = 1} = req.query;
+        const skip = (page - 1) * pageSize;
+        const [colaborators, totalColaborators] = await prisma.$transaction([
+            prisma.colaborador.findMany({
+                skip,
+                take: parseInt(pageSize)
+            }),
+            prisma.colaborador.count()
+        ])
+        const totalPages = Math.ceil(totalColaborators / pageSize);
         res.status(200).json({
             msg: `Se han obtenido los colaboradores`,
-            colaborators
-          });
+            colaborators,
+            pageSize,
+            page,
+            totalColaborators,
+            totalPages
+        });
     } catch (error) {
         console.log(error);
     }
@@ -46,10 +59,10 @@ export const putColaborator = async (req = Request, res = Response) => {
         EDAD = parseInt(EDAD);
         const { idColaborador } = req.params;
         const colaboratorFind = await prisma.colaborador.findUnique({
-            where: { IDCOLABORADOR: parseInt(idColaborador)}
+            where: { IDCOLABORADOR: parseInt(idColaborador) }
         })
         const colaboratorUpdate = await prisma.colaborador.update({
-            where:{ IDCOLABORADOR: parseInt(idColaborador)},
+            where: { IDCOLABORADOR: parseInt(idColaborador) },
             data: {
                 NOMBRE,
                 APELLIDO,
@@ -62,11 +75,11 @@ export const putColaborator = async (req = Request, res = Response) => {
 
         res.status(200).json({
             msg: "Se ha actualizado el colaborador",
-            find:"Colaborador encontrado",
+            find: "Colaborador encontrado",
             colaboratorFind,
-            update:"Colaborador actualizado", 
-            colaboratorUpdate, 
-          });
+            update: "Colaborador actualizado",
+            colaboratorUpdate,
+        });
     } catch (error) {
         console.log(error);
     }
@@ -76,12 +89,12 @@ export const deleteColaborator = async (req = Request, res = Response) => {
     try {
         const { idColaborador } = req.params;
         const colaboratorDelete = await prisma.colaborador.delete({
-            where:{ IDCOLABORADOR: parseInt(idColaborador)}
+            where: { IDCOLABORADOR: parseInt(idColaborador) }
         })
         res.status(200).json({
             msg: "Se ha eliminado el colaborador",
             colaboratorDelete
-          });
+        });
     } catch (error) {
         console.log(error);
     }
