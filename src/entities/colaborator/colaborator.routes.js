70c569da3bb838bea8ae3308480000a594d52dec
Kenneth Velásquez <kvelasquez-2019504@kinal.edu.gt>
2025-03-03 11:56:07 -0600

    correccion: validación de mayoria de edad

--- src/entities/colaborator/colaborator.routes.js
+++ src/entities/colaborator/colaborator.routes.js
@@ -29,7 +29,7 @@ router.post("/post/colaborador",[
     check("NOMBRE", "El nombre es obligatorio, menor o igual a 45 caracteres").not().isEmpty().isLength({max:45}),
     check("APELLIDO", "El apellido es obligatorio, menor o igual a 45 caracteres").not().isEmpty().isLength({max:45}),
     check("DIRECCION", "La direccion tiene que ser menor o igual a 45 caracteres").optional().isLength({max:45}),
-    check("EDAD", "La edad es obligatoria").isInt({ min: 1, max: 120 }).withMessage("La edad debe estar entre 1 y 120"),
+    check("EDAD", "La edad es obligatoria").isInt({ min: 18, max: 120 }).withMessage("La edad debe estar entre 18 y 120"),
     check("PROFESION", "La profesion es tiene que ser menor o igual a 45 caracteres").optional().isLength({max:45}),
     check("ESTADOCIVIL", "El estado civil tiene que ser menor o igual a 45 caracteres").optional().isLength({max:45}),
     validateFields
